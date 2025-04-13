a897d6fe3c632df8fe1f8a60853067011dfb6365
Guillem Poy <guplem@gmail.com>
2025-04-13 12:52:31 +0200

    small changes

--- boom/config/plugins/robojs/server.ts
+++ boom/config/plugins/robojs/server.ts
@@ -1,5 +1,10 @@
+const host = process.env.HOST || '0.0.0.0'
+const port = parseInt(process.env.PORT || '8080', 10)
+
+console.log(`Server attempting to bind to ${host}:${port}`)
+
 export default {
 	cors: true,
-	host: process.env.HOST || '0.0.0.0', // use HOST from env or fallback to all network interfaces
-	port: parseInt(process.env.PORT || '8080', 10) // use PORT from env or fallback to 8080
+	host,
+	port
 }
