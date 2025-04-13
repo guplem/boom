ea37824414fbc688979b0f00baccab2903ae5585
Guillem Poy <guplem@gmail.com>
2025-04-13 09:20:48 +0200

    Server configuration

--- boom/config/plugins/robojs/server.ts
+++ boom/config/plugins/robojs/server.ts
@@ -1,3 +1,5 @@
 export default {
-	cors: true
+	cors: true,
+	host: '0.0.0.0', // listen on all network interfaces rather than just localhost
+	port: parseInt(process.env.PORT || '8080', 10) // listen on port 8080 or the port specified in the environment variable PORT
 }
