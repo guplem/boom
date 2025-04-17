248c501959d7b724a2bcc45a3127d569ee83cd93
Guillem Poy <guplem@gmail.com>
2025-04-17 07:36:19 +0200

    Ensuring versions are compatible

--- boom/src/App.tsx
+++ boom/src/App.tsx
@@ -1,34 +1,42 @@
 import { SyncContextProvider, useSyncState } from "@robojs/sync";
+import { JSX } from "react";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useSyncState(0, ["counter"]);
-
+export function App(): JSX.Element {
   return (
     <SyncContextProvider>
-      <>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <p>Hello World!</p>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p>Hello World!</p>
+      <Counter />
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </SyncContextProvider>
   );
 }
 
-export default App;
+function Counter(): JSX.Element {
+  const [count, setCount] = useSyncState(0, ["counter"]);
+
+  const handleIncrement = (): void => {
+    console.log("Incrementing count. Current count:", count);
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="card">
+      <button onClick={handleIncrement}>count is {count}</button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
