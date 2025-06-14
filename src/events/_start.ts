397de5a46edb60139d7b9cc09799439629a41172
Guillem Poy <guplem@gmail.com>
2025-06-14 17:43:21 +0200

    Update _start.ts

--- src/events/_start.ts
+++ src/events/_start.ts
@@ -1,10 +1,10 @@
-import { Cron } from '@robojs/cron';
+// import { Cron } from '@robojs/cron';
 import { logger } from 'robo.js/logger.js';
 
 export default (): void => {
 	logger.info('🚀 Starting cron jobs...');
 
-	Cron('*/10 * * * * *', (): void => {
-		logger.info(`⏰ Cron job executed at ${new Date().toISOString()}`);
-	});
+	// Cron('*/10 * * * * *', (): void => {
+	// 	logger.info(`⏰ Cron job executed at ${new Date().toISOString()}`);
+	// });
 };
