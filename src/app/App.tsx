66f7952805408bec83e62482238e749879a8b23c
Guillem Poy <11029629+guplem@users.noreply.github.com>
2025-06-14 16:52:04 +0200

    Initialize sync-web-app

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import CounterPage from '@/app/modules/counter/Page';
+import RoomPage from '@/app/modules/room/Page';
+import { RoomStore, RoomStoreType } from '@/app/modules/room/Store';
+import { JSX } from 'react';
+
+// The
+export default function App(): JSX.Element {
+	const { room }: RoomStoreType = RoomStore();
+
+	return (
+		<div
+			className='noisyBackground'
+			style={{
+				backgroundColor: 'var(--background-for-noise)',
+				overflow: 'auto',
+				minHeight: '100vh',
+			}}
+		>
+			{!room ? <RoomPage /> : <CounterPage />}
+		</div>
+	);
+}
